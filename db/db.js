7256cb6ba7c72a7509b11613e7b3d3595cdcb35b
bansalbathgallery <bansalbathgallery@gmail.com>
2025-12-30 19:42:03 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,15 +1,11 @@
 const Sequelize = require('sequelize');
  config = require('../config/db');
-console.log('config :',config)
+//console.log('config :',config)
 const models = require('./models');
 
 // const firebase = require('firebase');
 // const firebaseApp = firebase.initializeApp(config.firebase);
-console.log('connection : ',{
-  host: config.production.host,
-  dialect: config.production.dialect,
-  pool: config.production.pool
-  })
+
  db = new Sequelize(config.production.database, config.production.username, config.production.password,
    {
   host: config.production.host,
