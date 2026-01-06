172efdace40fd0a9e0094fdfd4e71b9d0abebf5f
bansalbathgallery <bansalbathgallery@gmail.com>
2026-01-06 16:18:53 +0530

    Update development.js

--- config/development.js
+++ config/development.js
@@ -1,7 +1,7 @@
 const db = require('./db');
 module.exports = {
 	db: db.production,
-	PORT:9062,
+	PORT:9066,
 	mediapath: 'public/company/',
 	dirnames : ['images', 'thumbnails', 'audios', 'videos','documents'],
 	jwtToken: 'PGSGFUISBHS=^$#*(%^#',
